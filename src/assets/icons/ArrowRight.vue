66cea7d37c75ffbd90a9715d9d4196e74807b475
Shohzod <xorozovshohzod@gmail.com>
2023-11-01 05:44:12 +0300

    initial

---
+++ src/assets/icons/ArrowRight.vue
@@ -0,0 +1,17 @@
+<template>
+  <svg
+    width="26"
+    height="14"
+    viewBox="0 0 26 14"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M19.6667 1.66663L25 6.99996M25 6.99996L19.6667 12.3333M25 6.99996L1 6.99996"
+      stroke="#374151"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+  </svg>
+</template>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,29 @@
+<script setup>
+import Logo from "../assets/icons/Logo.vue";
+</script>
+<template>
+  <nav>
+    <div class="container">
+      <div class="navbar__flex">
+        <a href="#"><Logo /></a>
+        <ul>
+          <li><a href="#">О нас</a></li>
+          <li><a href="#">Edu Coozin</a></li>
+          <li><a href="#">Почему Coozin?</a></li>
+          <li><a href="#">Мероприятия</a></li>
+        </ul>
+
+        <div class="navbar__btn__group">
+          <button>UZ</button>
+          <button>Присоединиться</button>
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
+<style scoped>
+li a {
+  color: var(--orange-color);
+  font-family: var(--inter-color);
+}
+</style>
